c2eb5dad06799a420873283923af183a54099bcc
Danilo Cruz <danilo.oliveiradacruz@gmail.com>
2021-01-02 23:01:55 -0300

    Exibindo dados dinamicamente com javascript

---
+++ projeto-fetch-API/cliente.js
@@ -0,0 +1,29 @@
+const informacoesClientes = [
+    {
+        cpf: 18875539081,
+        nome: "Jairo"
+    },
+    {
+        cpf: 18875539081,
+        nome: "Monica"
+    }
+];
+
+const corpoTabela = document.querySelector("[data-conteudo-tabela]");
+
+const exibeCliente = (cpf, nome) => {
+    const linha = document.createElement('tr');
+
+    const conteudoLinha = `    
+        <td>${cpf}</td>
+        <td>${nome}</td>
+    `;
+
+    linha.innerHTML = conteudoLinha;
+
+    return linha;
+}
+
+informacoesClientes.forEach(indice => {
+    corpoTabela.appendChild(exibeCliente(indice.cpf, indice.nome));
+});
