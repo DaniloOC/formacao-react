fca85ebd0ce0fd3f6d219e5535e4d43d5ea077cd
Danilo Cruz <danilo.oliveiradacruz@gmail.com>
2021-01-05 17:16:29 -0300

    Deletando cliente

--- projeto-fetch-API/listagem-cliente.js
+++ projeto-fetch-API/listagem-cliente.js
@@ -1,13 +1,20 @@
 const informacoesClientes = [];
 
+const removeCliente = id => {
+    if(confirm("Deseja excluir o cliente?")) {
+        deletaCliente(id);
+    }
+};
+
 const corpoTabela = document.querySelector("[data-conteudo-tabela]");
 
-const exibeCliente = (cpf, nome) => {
+const exibeCliente = (cpf, nome, id) => {
     const linha = document.createElement('tr');
 
     const conteudoLinha = `    
         <td>${cpf}</td>
         <td>${nome}</td>
+        <button type="button" class="btn btn-danger" onclick="removeCliente(${id})">Excluir</button>
     `;
 
     linha.innerHTML = conteudoLinha;
@@ -17,6 +24,6 @@ const exibeCliente = (cpf, nome) => {
 
 listarClientes().then(exibe => {
     exibe.forEach(indice => {
-        corpoTabela.appendChild(exibeCliente(indice.cpf, indice.nome));
+        corpoTabela.appendChild(exibeCliente(indice.cpf, indice.nome, indice.id));
     })
 });
