24358076758bc245ff6ba011551aae8c0e2e9873
Danilo Cruz <danilo.oliveiradacruz@gmail.com>
2021-01-13 21:46:00 -0300

    Observable

--- ceep/src/App.js
+++ ceep/src/App.js
@@ -4,44 +4,23 @@ import ListaDeNotas from './components/ListaDeNotas'
 import ListaDeCategorias from './components/ListaDeCategorias'
 import './assets/App.css'
 import './assets/index.css'
+import Categorias from './dados/Categorias'
+import ArrayDeNotas from './dados/Notas'
 
 class App extends Component {
   constructor () {
     super()
-    this.state = {
-      notas: [],
-      categorias: []
-    }
-  }
-
-  criarNota (titulo, texto, categoria) {
-    const novaNota = { titulo, texto, categoria }
-    const novoArrayNotas = [...this.state.notas, novaNota]
-    const novoEstado = {
-      notas: novoArrayNotas
-    }
-    this.setState(novoEstado)
-  }
-
-  deletarNota (index) {
-    const arrayNotas = this.state.notas
-    arrayNotas.splice(index, 1)
-    this.setState({ notas: arrayNotas })
-  }
-
-  adicionarCategoria (nomeCategoria) {
-    const novoArrayCategoria = [...this.state.categorias, nomeCategoria]
-    const novoEstado = { ...this.state, categorias: novoArrayCategoria }
-    this.setState(novoEstado)
+    this.categorias = new Categorias()
+    this.notas = new ArrayDeNotas()
   }
 
   render () {
     return (
       <section className="conteudo">
-        <FormularioCadastro categorias={this.state.categorias} criarNota={this.criarNota.bind(this)}/>
+        <FormularioCadastro categorias={this.categorias.categorias} criarNota={this.notas.adicionarNota}/>
         <main className='conteudo-principal'>
-          <ListaDeCategorias adicionarCategoria={this.adicionarCategoria.bind(this)} categorias={this.state.categorias}/>
-          <ListaDeNotas apagarNota={this.deletarNota.bind(this)} notas={this.state.notas}/>
+          <ListaDeCategorias adicionarCategoria={this.categorias.adicionarCategoria} categorias={this.categorias.categorias}/>
+          <ListaDeNotas apagarNota={this.notas.apagarNota} notas={this.notas.notas}/>
         </main>
       </section>
     )
