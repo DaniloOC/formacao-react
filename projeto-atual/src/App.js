0683d6eb8c04c292980351846a4893be2cfe746f
Danilo Cruz <danilo.oliveiradacruz@gmail.com>
2021-01-22 15:28:23 -0300

    Utilizando o Material UI

--- projeto-atual/src/App.js
+++ projeto-atual/src/App.js
@@ -1,13 +1,15 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import './App.css';
 import FormularioCadastro from './components/FormularioCadastro/FormularioCadastro';
+import { Container, Typography } from "@material-ui/core";
+import 'fontsource-roboto'
 
 function App() {
   return (
-    <Fragment>
-      <h1>Formulário de cadastro</h1>
+    <Container component="article" maxWidth="sm">
+      <Typography variant="h3" component="h1" >Formulário de cadastro</Typography>
       <FormularioCadastro />
-    </Fragment>
+    </Container>
   );
 }
 
